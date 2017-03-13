8d05d17c88bb913c9f9f9742cb43c0ae5d633d08
Vicente González Ruiz <vruiz@ual.es>
2017-03-13 18:07:55 +0100

    .

---
+++ debug_me_2.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+#import pydb, sys
+#sys.excepthook = pydb.exception_hook
+
+def call_me():
+    print("Debug me with \"python -i debug_me.py\"")
+    print("After the run-time error, you should have access to the \"a\" variable")
+    a=1
+    c=0
+    import ipdb; ipdb.set_trace()
+    d=1/c
+    print("This code is never reached")
+    a=2
+
+b=1
+
+call_me()
