18cafd9e60f2aadb996f6c250cb0f3a105b432eb
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2017-03-18 10:16:33 +0100

    .

--- debug_me.py
+++ debug_me.py
@@ -1,8 +1,5 @@
 #!/usr/bin/env python3
 
-#import pydb, sys
-#sys.excepthook = pydb.exception_hook
-
 def call_me():
     print("Debug me with \"python -i debug_me.py\"")
     print("After the run-time error, you should have access to the \"a\" variable")
