7db6ac7aee5732a51a01b438e34852a57ae8c584
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2018-12-18 16:19:00 +0100

    .

--- Cython/basic/setup.py
+++ Cython/basic/setup.py
@@ -2,5 +2,5 @@ from distutils.core import setup
 from Cython.Build import cythonize
 
 setup(
-    ext_modules=cythonize("sum_array_lib__cython.pyx"),
+    ext_modules=cythonize("sum_array_lib.pyx"),
 )
