f4f949756d3bca6f87f6e2aaea7af3ff05db62d4
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2020-06-14 19:54:52 +0200

    .

---
+++ Workshops/PyGame/01-simple_screen.py
@@ -0,0 +1,11 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800, 600))
+
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
