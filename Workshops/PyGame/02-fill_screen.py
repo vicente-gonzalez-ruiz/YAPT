b80b054de1d941f515054d7102a0c7b01d28999c
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2020-06-17 13:10:02 +0200

    .

--- Workshops/PyGame/02-fill_screen.py
+++ Workshops/PyGame/02-fill_screen.py
@@ -3,10 +3,13 @@ import pygame
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Screen Name")
+pygame.display.set_caption("Fill Screen with a Color")
 
 running = True
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+            
+    screen.fill((100,200,100)) # RGB, 8 bits/component
+    pygame.display.update()
