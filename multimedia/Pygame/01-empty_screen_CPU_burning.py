45c149842944441fd8f79785e565b88d2209e2ce
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2020-08-16 10:56:00 +0200

    .

--- multimedia/Pygame/01-empty_screen_CPU_burning.py
+++ multimedia/Pygame/01-empty_screen_CPU_burning.py
@@ -1,11 +1,11 @@
 import pygame
 
 pygame.init()
-width = 800
-height = 600
-screen_size = (width, height)
+screen_width = 800
+screen_height = 600
+screen_size = (screen_width, screen_height)
 screen = pygame.display.set_mode(screen_size)
-pygame.display.set_caption("Simple Screen")
+pygame.display.set_caption("Empty Screen")
 
 running = True
 while running:
--- multimedia/Pygame/02-plot_pixel_and_wait_for_exit.py
+++ multimedia/Pygame/02-plot_pixel_and_wait_for_exit.py
@@ -2,20 +2,20 @@ import pygame
 import my_colors as color
 
 pygame.init()
-width = 800
-height = 600
-screen_size = (width, height)
+screen_width = 800
+screen_height = 600
+screen_size = (screen_width, screen_height)
 screen = pygame.display.set_mode(screen_size)
 pygame.display.set_caption("Search the green pixel at the coordinates (x=10, y=100)")
 
 running = True
 while running:
-    event = pygame.event.wait()
-    if event.type == pygame.QUIT:
-        running = False
     screen.set_at((1, 1), color.white)
     screen.set_at((10, 100), color.green)
     pygame.display.update()
+    event = pygame.event.wait()  # Wait for an (input) event
+    if event.type == pygame.QUIT:
+        running = False
 
 pygame.quit()
 print("Goodbye!")
