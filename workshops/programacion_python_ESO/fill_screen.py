1c6ac08c6218810757a8b56ed16abe577a825fbe
Vicente González Ruiz <vicente.gonzalez.ruiz@gmail.com>
2020-08-17 13:41:21 +0200

    .

--- workshops/programacion_python_ESO/fill_screen.py
+++ workshops/programacion_python_ESO/fill_screen.py
@@ -1,11 +1,6 @@
 import pygame
-#import colors as color
-print(__name__)
-print(__package__)
-#from .colors import colors as color
-#import colors.colors as color
-#import .programacion_python_ESO.colors as color
-from ..programacion_python_ESO import colors as color
+from lib import colors as color
+
 pygame.init()
 
 screen = pygame.display.set_mode((800, 600))
