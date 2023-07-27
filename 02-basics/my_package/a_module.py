0fdae341dab57bb8d7d89357784d3bc1266f32b4
Vicente González-Ruiz <vruiz@ual.es>
2023-07-27 07:16:50 +0200

    .

--- 02-basics/my_package/a_module.py
+++ 02-basics/my_package/a_module.py
@@ -4,5 +4,5 @@ print("a_module: Hi from my_package/" + __name__ + ".py!")
 if __name__ == "__main__":
   print("a_module: I was invoked from a script.")
 else:
-  print("a_module: I was invoked from a Pyton module (probably using 'import').") 
+  print("a_module: I was invoked from a Python module (probably using 'import').") 
 print("a_module: My name is =", __name__)
